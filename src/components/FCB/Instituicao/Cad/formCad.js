0b05d67853143b71949eaa4bf248d75e704ba486
Junior-Oliveira <franciscoanto@gmail.com>
2023-02-21 17:14:41 -0300

    #6 🚸Dado estilo na troca de componentes

---
+++ src/components/FCB/Instituicao/Cad/formCad.js
@@ -0,0 +1,175 @@
+import {useState} from 'react';
+import { useForm } from "react-hook-form";
+import { api } from 'services/Api';
+
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import Input from "@mui/material/Input";
+import Icon from "@mui/material/Icon";
+import FormHelperText from "@mui/material/FormHelperText";
+// Material Dashboard 2 React Components
+import MDBox from "components/MDBox";
+import MDButton from "components/MDButton";
+import MDAlert from "components/MDAlert";
+//COMPONENT CUSTOM
+import PhoneCuston from "components/FCB/Util/PhoneCuston";
+
+import "./cad.css";
+
+function FormCadInstitution(props) {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm();
+  const [phone, setPhone] = useState("");
+  const [verifyPass, setVerifyPass] = useState(false);
+  
+  const onPhoneChange = (e: any) => setPhone(e.target.value);
+
+  const onSubmit = (data) => {
+    data['phone'] = phone
+    data['username'] = data.alias
+    data['birthday'] = '1984-11-05'
+    if(data.password !== data.conf_pass) {
+        console.log('senhas nao confere')
+        setVerifyPass(true)
+        return false;
+    }else{
+        setVerifyPass(false)
+        api.post('auth/local/register', data)
+        .then((res) => {
+            console.log({res})
+            props.stepCadIns(true)
+        })
+        .catch((err) => {
+            console.log({err})
+        })
+    }
+    console.log(data)
+   
+   
+  };
+  
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+    <MDBox
+      sx={{
+        mt: 2,
+        mb: 2,
+      }}
+    >
+      <div>
+        <InputLabel htmlFor="name-complet">
+          Nome Completo
+        </InputLabel>
+        <Input
+          id="name-complet"
+          fullWidth
+          aria-invalid={errors.name ? "true" : "false"}
+          {...register("name", {
+            required: "O Nome completo é obrigatório",
+          })}
+          startAdornment={
+            <InputAdornment position="start">
+              <Icon>person_icon</Icon>
+            </InputAdornment>
+          }
+        />
+        <FormHelperText className="Mui-error">
+          {errors.name?.message}
+        </FormHelperText>
+        <InputLabel htmlFor="alias_user" className="mt-label">
+          Como é conhecido ou apelido
+        </InputLabel>
+        <Input
+          id="alias_user"
+          fullWidth
+          {...register("alias")}
+          startAdornment={
+            <InputAdornment position="start">
+              <Icon>person_icon</Icon>
+            </InputAdornment>
+          }
+        />
+        <InputLabel htmlFor="email" className="mt-label">
+          E-mail
+        </InputLabel>
+        <Input
+          id="email"
+          fullWidth
+          {...register("email", {
+            required: "O E-mail é",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "O formato de e-mail está incorreto",
+            },
+          })}
+          startAdornment={
+            <InputAdornment position="start">
+              <Icon>alternate_email_icon</Icon>
+            </InputAdornment>
+          }
+        />
+        <FormHelperText className="Mui-error">
+          {errors.email?.message}
+        </FormHelperText>
+        <InputLabel htmlFor="pass" className="mt-label">
+          Senha
+        </InputLabel>
+        <Input
+          id="pass"
+          fullWidth
+          {...register("password", { required: true })}
+          startAdornment={
+            <InputAdornment position="start">
+              <Icon>lock_icon</Icon>
+            </InputAdornment>
+          }
+        />
+        <InputLabel htmlFor="conf_pass" className="mt-label">
+          Confirmar Senha
+        </InputLabel>
+        <Input
+          id="conf_pass"
+          fullWidth
+          {...register("conf_pass", { required: true })}
+          startAdornment={
+            <InputAdornment position="start">
+              <Icon>lock_icon</Icon>
+            </InputAdornment>
+          }
+        />
+        <InputLabel htmlFor="phone" className="mt-label">
+          Celular ou Whatsapp
+        </InputLabel>
+        <Input
+          id="phone"
+          fullWidth
+          value={phone}
+          onBlur={onPhoneChange}
+          inputComponent={PhoneCuston}
+          startAdornment={
+            <InputAdornment position="start">
+              <Icon>smartphone_icon</Icon>
+            </InputAdornment>
+          }
+        />
+        {verifyPass && (
+          <MDAlert color="error" dismissible>
+            <Icon fontSize="small">thumb_up</Icon>&nbsp; Senhas
+            não confere
+          </MDAlert>
+        )}
+        <MDButton type="submit" color="secondary" fullWidth>
+          Próximo
+        </MDButton>
+      </div>
+    </MDBox>
+  </form>
+  );
+}
+
+export default FormCadInstitution;
--- src/components/FCB/Instituicao/Cad/index.js
+++ src/components/FCB/Instituicao/Cad/index.js
@@ -1,15 +1,16 @@
-import {useState} from "react";
-import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+
 
 import Icon from "@mui/material/Icon";
 import PageLayout from "examples/LayoutContainers/PageLayout";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
-import FormHelperText from '@mui/material/FormHelperText'
+import FormHelperText from "@mui/material/FormHelperText";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
-import FormControl from "@mui/material/FormControl";
 import Input from "@mui/material/Input";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 // Material Dashboard 2 React Components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
@@ -17,36 +18,46 @@ import MDButton from "components/MDButton";
 import MDAlert from "components/MDAlert";
 //COMPONENT CUSTOM
 import PhoneCuston from "components/FCB/Util/PhoneCuston";
+import FormCadInstitution from "./formCad";
 //style css
 import "./cad.css";
+import "animate.css";
 import { api } from "services/Api";
 
 function Instituicao() {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const [phone, setPhone] = useState("");
-    const [verifyPass, setVerifyPass] = useState(false)
 
-    const onPhoneChange = (e: any) => setPhone(e.target.value);
+  const [phone, setPhone] = useState("");
+  const [verifyPass, setVerifyPass] = useState(false);
+  const [showCadUserInstitution, setShowCadUserInstitution] = useState(true);
+  const [showInstituition, setShowInstituition] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const onSubmit = data => {
-        data['phone'] = phone
-        data['username'] = data.alias
-        data['birthday'] = '1984-11-05'
-        if(data.password !== data.conf_pass) {
-            console.log('senhas nao confere')
-            setVerifyPass(true)
-        }else{
-            setVerifyPass(false)
-        }
-        console.log(data)
-        api.post('auth/local/register', data)
-        .then((res) => {
-            console.log({res})
-        })
-        .catch((err) => {
-            console.log({err})
-        })
-    };
+  const showPri = () => {
+    console.log("showPri");
+    setShowInstituition(false);
+    setShowCadUserInstitution(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+    setTimeout(() => {
+      setOpen(false);
+      setShowInstituition(true);
+      setShowCadUserInstitution(false);
+    }, 1000);
+  };
+
+  function stepCadIns(success) {
+    if(success) {
+      handleToggle()
+    }else{
+      handleClose()
+    }
+  }
+
   return (
     <PageLayout>
       <Container>
@@ -82,121 +93,60 @@ function Instituicao() {
                     corretas. Vamos lá!!!
                   </MDTypography>
                 </MDBox>
-                <form  onSubmit={handleSubmit(onSubmit)}>
-                <MDBox                   
-                    sx={{
-                     mt: 2,
-                     mb: 2,
-                    }}
+                {showCadUserInstitution && (
+                 <FormCadInstitution stepCadIns={stepCadIns}/>
+                )}
+                <MDButton
+                  type="submit"
+                  color="secondary"
+                  fullWidth
+                  onClick={showPri}
                 >
-                    <div>
-                      <InputLabel htmlFor="name-complet">
-                        Nome Completo
-                      </InputLabel>
-                      <Input
-                        id="name-complet"
-                        fullWidth
-                        aria-invalid={errors.name ? "true" : "false"} 
-                        {...register("name", {required: "O Nome completo é obrigatório"})}
-                        startAdornment={
-                          <InputAdornment position="start">
-                            <Icon>person_icon</Icon>
-                          </InputAdornment>
-                        }
-                      />
-                      <FormHelperText className="Mui-error">{errors.name?.message}</FormHelperText>
-                      <InputLabel htmlFor="alias_user" className="mt-label">
-                       Como é conhecido ou apelido
-                      </InputLabel>
-                      <Input
-                        id="alias_user"
-                        fullWidth
-                        {...register("alias")}
-                        startAdornment={
-                          <InputAdornment position="start">
-                            <Icon>person_icon</Icon>
-                          </InputAdornment>
-                        }
-                      />
-                       <InputLabel htmlFor="email" className="mt-label">
-                       E-mail
-                       </InputLabel>
-                      <Input
-                        id="email"
-                        fullWidth
-                        {...register("email", {required: "O E-mail é", pattern: {
-                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                            message: "O formato de e-mail está incorreto"
-                          }})}
-                        startAdornment={
-                          <InputAdornment position="start">
-                            <Icon>alternate_email_icon</Icon>
-                          </InputAdornment>
-                        }
-                      />
-                       <FormHelperText className="Mui-error">{errors.email?.message}</FormHelperText>
-                       <InputLabel htmlFor="pass" className="mt-label">
-                       Senha
-                      </InputLabel>
-                      <Input
-                        id="pass"
-                        fullWidth
-                        {...register("password", {required: true})}
-                        startAdornment={
-                          <InputAdornment position="start">
-                            <Icon>lock_icon</Icon>
-                          </InputAdornment>
-                        }
-                      />
-                       <InputLabel htmlFor="conf_pass" className="mt-label">
-                       Confirmar Senha
-                       </InputLabel>
-                       <Input
-                        id="conf_pass"
-                        fullWidth
-                        {...register("conf_pass", {required: true})}
-                        startAdornment={
-                          <InputAdornment position="start">
-                            <Icon>lock_icon</Icon>
-                          </InputAdornment>
-                        }
-                       />
-                       <InputLabel htmlFor="phone" className="mt-label">
-                       Celular ou Whatsapp
-                       </InputLabel>
-                       <Input
-                        id="phone"
-                        fullWidth
-                        value={phone} 
-                        onBlur={onPhoneChange}
-                        inputComponent={PhoneCuston}
-                        startAdornment={
-                          <InputAdornment position="start">
-                            <Icon>smartphone_icon</Icon>
-                          </InputAdornment>
-                        }
-                       />
-                    </div>
+                  show
+                </MDButton>
+
+                <Backdrop
+                  sx={{
+                    color: "#fff",
+                    zIndex: (theme) => theme.zIndex.drawer + 1,
+                  }}
+                  open={open}
+                  onClick={handleClose}
+                >
+                  <CircularProgress color="inherit" />
+                </Backdrop>
+
+                {showInstituition && (
+                  <MDBox
+                    sx={{
+                      mt: 2,
+                      mb: 2,
+                    }}
+                    className="animate__animated animate__backInUp"
+                  >
+                    <InputLabel htmlFor="institution">
+                      Nome da Instituição
+                    </InputLabel>
+                    <Input
+                      id="institution"
+                      fullWidth
+                      startAdornment={
+                        <InputAdornment position="start">
+                          <Icon>person_icon</Icon>
+                        </InputAdornment>
+                      }
+                    />
+                    <FormHelperText className="Mui-error">
+                     Ocorreu um erro inesperado
+                    </FormHelperText>
                   </MDBox>
-                  {verifyPass && (
-                    <MDAlert color="error" dismissible>
-                        <Icon fontSize="small">thumb_up</Icon>&nbsp;
-                        Senhas não confere</MDAlert>
-                  )}
-                  <MDButton
-                    type="submit"
-                    color="secondary"
-                    fullWidth
-                   
-                  >Próximo</MDButton>
-                  </form>
+                )}
               </MDBox>
             </Grid>
             <MDBox mt={3} mb={1} textAlign="center">
               <MDTypography variant="button" color="text">
                 Já possui um cadastro?{" "}
                 <MDTypography
-                
                   to="/authentication/sign-up"
                   variant="button"
                   color="info"
@@ -209,7 +159,6 @@ function Instituicao() {
             </MDBox>
           </Grid>
           <Grid item xs={0} sm={2}></Grid>
-         
         </Grid>
       </Container>
     </PageLayout>
