f668fe7f4a3b610464ed4753884741e957959234
Junior-Oliveira <franciscoanto@gmail.com>
2023-02-22 12:08:15 -0300

    Alterado link de cadastro instituicao

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -146,7 +146,7 @@ function Basic() {
                 Don&apos;t have an account?{" "}
                 <MDTypography
                   component={Link}
-                  to="/authentication/sign-up"
+                  to="/cadastro-instituicao"
                   variant="button"
                   color="info"
                   fontWeight="medium"
